210e0e0ca65b9dbc4bc6de98299112dfa05b2c8b
KIRTI-GITHUB <kirtirai7549@gmail.com>
2026-05-06 16:47:39 +0530

    bug fix

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,7 +10,7 @@ const Todo = () => {
   };
 
   const deleteItem = (i) => {
-    const newArr = list.filter((item) => item !== value);
+    const newArr = list.filter((item) => item !== i);
     setList(newArr);
   };
 
