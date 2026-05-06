45d78ce0e7a4497c4bdb6130463158cd4823d34c
KIRTI-GITHUB <kirtirai7549@gmail.com>
2026-05-06 16:30:06 +0530

    functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import Todo from "./Todo";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <Todo />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
